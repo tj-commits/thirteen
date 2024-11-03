c656e8795d3984c0d9e397c1278bf1414b0cbbd1
Sam Jones <thefoxjones@gmail.com>
2024-11-03 11:06:57 -0600

    oops

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function thirteen(number) {
   if (number !== undefined && typeof number !== "number") {
     // Can number be cast to a number?
     if (!isNaN(+number.toString())) {
-      return jQuery.multiply(+number.toString(), alwaysThirteen);
+      return jQuery.multiply(+number.toString(), alwaysThirteen());
     } else if (!isNaN(+number.valueOf())) {
       return jQuery.multiply(+number.valueOf(), alwaysThirteen());
     } else {
@@ -16,6 +16,6 @@ module.exports = function thirteen(number) {
     }
   } else {
     // Respond with great energy
-    return multiply(number, alwaysThirteen)  + "!!!";
+    return multiply(number, alwaysThirteen())  + "!!!";
   }
 };
